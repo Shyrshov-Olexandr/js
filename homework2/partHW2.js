54dd988883c4f1889f81aa177a7c215e9d7cc189
Shyrshov-Olexandr <shyrshovolexandr@gmail.com>
2022-11-03 17:33:21 +0200

    Initial commit

---
+++ homework2/partHW2.js
@@ -0,0 +1,13 @@
+//let array = ['own', 'two', 10, true, 'three', 'four', 3, 7, 9]
+//console.log(array)
+let autors =
+  {
+    name: 'fscfrc',
+    age: 40
+  }
+let literatur = [
+  {title: 'fvjzlv', pageCount: 10, genre: 'dlacjksd', autors},
+  {title: 'dklsjcajkn', pageCount: 30, genre: 'rejklnl', autors},
+  {title: 'asdcjlsd', pageCount: 50, genre: 'ajkena', autors}
+]
+console.log(literatur)
